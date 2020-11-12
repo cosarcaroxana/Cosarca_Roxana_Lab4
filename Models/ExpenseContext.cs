0a2f86ee980fb5c522a43a0c423512b0bd126404
cosarcaroxana <roxanacosarca@gmail.com>
2020-11-12 19:26:42 +0200

    lab4

--- Models/ExpenseContext.cs
+++ Models/ExpenseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Cosarca_Roxana_Lab4.Models;
 
 
 namespace Cosarca_Roxana_Lab4.Models
@@ -15,5 +16,6 @@ namespace Cosarca_Roxana_Lab4.Models
         }
         public DbSet<Expense> Expense { get; set; }
         public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+        public DbSet<Budget> Budget { get; set; }
     }
 }
